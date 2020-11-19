ba467fe5153e064f14103aa814e3f52eaa31029d
tanvir <tanvirgeek@gmail.com>
2020-11-19 06:58:22 +0600

    Initial Commit

---
+++ DogGram/DogGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGramApp.swift
+//  DogGram
+//
+//  Created by MD Tanvir Alam on 19/11/20.
+//
+
+import SwiftUI
+
+@main
+struct DogGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
