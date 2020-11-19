ec012cffce8577255c1427a36059de37c1d7f3ee
tanvir <tanvirgeek@gmail.com>
2020-11-19 08:43:44 +0600

    Day1

---
+++ DogGram/Helpers/Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  Extensions.swift
+//  DogGram
+//
+//  Created by MD Tanvir Alam on 19/11/20.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    struct Mytheme {
+        
+        static var purpleColor:Color{
+            return Color("ColorPurple")
+        }
+        static var yellowColor:Color{
+            return Color("ColorYellow")
+        }
+        static var baigeColor:Color{
+            return Color("ColorBaige")
+        }
+        
+    }
+}
---
+++ DogGram/Views/Screens/Home.swift
@@ -0,0 +1,41 @@
+//
+//  Home.swift
+//  DogGram
+//
+//  Created by MD Tanvir Alam on 19/11/20.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        TabView(){
+            Text("Screen1")
+                .tabItem {
+                    Image(systemName: "book.fill")
+                    Text("Feed")
+                }
+            Text("Screen2")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Browse")
+                }
+            Text("Screen3")
+                .tabItem {
+                    Image(systemName: "square.and.arrow.up")
+                    Text("Upload")
+                }
+            Text("Screen4")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Profile")
+                }
+        }.accentColor(Color.Mytheme.purpleColor)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
